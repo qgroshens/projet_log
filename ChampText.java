46c61b3f11fd0a0c7cd53b05894fe92c667a00da
ezor1 <edouard.zerdoun@gmail.com>
2015-03-16 17:57:26 +0100

    plop

--- ChampText.java
+++ ChampText.java
@@ -3,28 +3,23 @@ package projet_log;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.text.NumberFormat;
 
-import javax.swing.JTextField;
+import javax.swing.JFormattedTextField;
 
-public class ChampText extends JTextField implements ActionListener {
-	static final long serialVersionUID = 1;
+public class ChampText extends JFormattedTextField {
 	
+	static final long serialVersionUID = 1;
 	private Fond fen_reglage;
 	private Fond fen_simulation;
 	
 
 	public ChampText(Fond f_reglage, Fond f_simulation, String text_init ){
+		super(NumberFormat.getIntegerInstance());
 		this.fen_reglage = f_reglage;
 		this.fen_simulation=f_simulation;
 		this.setPreferredSize(new Dimension(100,20));
 		this.setText(text_init);
 	}
 	
-	public void test(){
-		System.out.println(""+this.getText());
-	}
-	
-	public void actionPerformed(ActionEvent e){
-		
-	}
 }
