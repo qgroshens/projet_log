fdad5b6f7a1ade0996510a76ef2ae2fe07ba2742
shinversus <quentin.groshens@supelec.fr>
2015-03-26 17:56:47 +0100

    yuiu

--- ThreadSimu.java
+++ ThreadSimu.java
@@ -23,8 +23,9 @@ public class ThreadSimu extends Thread {
 		catch(InterruptedException e){
 		}
 		//attente = !fenetre_reglage.get_gomain();
-		fenetre_reglage
-		route route=new route(6,60,20,100,0.2,0.00,0.0000,0.8);//int vmax, int longueur,int nb_voiture, int nb_itt, double p,double p2
+		Parametrage param =fenetre_reglage.get_Parametrage();
+	
+		route route=new route(param.get_vitesse_max(),param.get_taille_route(),param.get_nbvoiture(),param.get_nbstep(),param.get_proba_ralentir(),0.00,0.0000,0.8);//int vmax, int longueur,int nb_voiture, int nb_itt, double p,double p2
 		route.creation();
 		route.init_matrice_densite(60, 300);
 		Affichage affichage=new Affichage();
